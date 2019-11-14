2b6576865c5fc669bc673a80bdb1820f1d1912df
MKeiNll <levgirich@gmail.com>
2019-11-14 17:18:26 +0200

    remove redundant comments

--- back-end/src/Server.ts
+++ back-end/src/Server.ts
@@ -7,12 +7,8 @@ import BaseRouter from './routes';
 import { Request, Response } from 'express';
 import { jwtCookieProps } from '@shared';
 
-
-// Init express
 const app = express();
 
-
-// Add middleware/settings/routes to express.
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -20,28 +16,4 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', BaseRouter);
 
-
-/**
- * Serve front-end content.
- */
-// const viewsDir = path.join(__dirname, 'views');
-// app.set('views', viewsDir);
-// const staticDir = path.join(__dirname, 'public');
-// app.use(express.static(staticDir));
-
-// app.get('/', (req: Request, res: Response) => {
-//     res.sendFile('login.html', {root: viewsDir});
-// });
-
-// app.get('/users', (req: Request, res: Response) => {
-//     const jwt = req.signedCookies[jwtCookieProps.key];
-//     if (!jwt) {
-//         res.redirect('/');
-//     } else {
-//         res.sendFile('users.html', {root: viewsDir});
-//     }
-// });
-
-
-// Export express instance
 export default app;
