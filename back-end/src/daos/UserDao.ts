4ef1e9f5a3749f33baa17ccb0e5e0a342aef7de1
MKeiNll <levgirich@gmail.com>
2019-11-14 17:15:26 +0200

    change dao structure

---
+++ back-end/src/daos/UserDao.ts
@@ -0,0 +1,91 @@
+import { IUser } from "@entities";
+import { getRandomInt } from "@shared";
+import jsonfile from "jsonfile";
+
+interface IUserDao {
+  getOne: (email: string) => Promise<IUser | null>;
+  getAll: () => Promise<IUser[]>;
+  add: (user: IUser) => Promise<void>;
+  update: (user: IUser) => Promise<void>;
+  delete: (id: number) => Promise<void>;
+}
+
+class UserDb {
+  private readonly dbFilePath = "src/daos/UserDb.json";
+
+  protected openDb(): Promise<any> {
+    return jsonfile.readFile(this.dbFilePath);
+  }
+
+  protected saveDb(db: any): Promise<any> {
+    return jsonfile.writeFile(this.dbFilePath, db);
+  }
+}
+
+export class UserDao extends UserDb implements IUserDao {
+  public async getOne(email: string): Promise<IUser | null> {
+    try {
+      const db = await super.openDb();
+      for (const user of db.users) {
+        if (user.email === email) {
+          return user;
+        }
+      }
+      return null;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  public async getAll(): Promise<IUser[]> {
+    try {
+      const db = await super.openDb();
+      return db.users;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  public async add(user: IUser): Promise<void> {
+    try {
+      const db = await super.openDb();
+      user.id = getRandomInt();
+      db.users.push(user);
+      await super.saveDb(db);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  public async update(user: IUser): Promise<void> {
+    try {
+      const db = await super.openDb();
+      for (let i = 0; i < db.users.length; i++) {
+        if (db.users[i].id === user.id) {
+          db.users[i] = user;
+          await super.saveDb(db);
+          return;
+        }
+      }
+      throw new Error("User not found");
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  public async delete(id: number): Promise<void> {
+    try {
+      const db = await super.openDb();
+      for (let i = 0; i < db.users.length; i++) {
+        if (db.users[i].id === id) {
+          db.users.splice(i, 1);
+          await super.saveDb(db);
+          return;
+        }
+      }
+      throw new Error("User not found");
+    } catch (err) {
+      throw err;
+    }
+  }
+}
